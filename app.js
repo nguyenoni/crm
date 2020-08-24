0eb6c85bb09a1a6b36f029e33e55fdb0f1d39e1b
nguyen oni <an64440@gmail.com>
2020-08-24 10:45:00 +0700

    function filter, update refesh page

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const url_api = "https://script.google.com/macros/s/AKfycbwvWDwAkJu--B1sJfMS3jJS
 
 
 $(document).ready(function () {
-  // $('select').formSelect();
+  // function show data
+
+
 
   // Fakes the loading setting a timeout
   setTimeout(function () {
@@ -12,89 +14,9 @@ $(document).ready(function () {
   }, 3500);
   // Event load modal add new bill order
   // $('.create-bill-order').on("click", )
-  $('#exampleModal').on('show.bs.modal', function (event) {
-    const order_id = $(event.relatedTarget).data('val');
-    // set value to input form
-    let data_find = JSON.parse(localStorage.getItem("data"));
-    let obj = data_find.filter(item => item.id_customer === order_id);
-    $('.customer-id').val(obj[0].id_customer);
-    $('.customer-name').val(obj[0].name);
-    $('.phone-number').val(obj[0].phone);
-    $('.name-sale').val(obj[0].name_sale_process);
-    $('.row').val(obj[0].row);
-  });
 
-  // Clear all value in modal when modal hide
-  $("#exampleModal").on('hide.bs.modal', function(){
-    $('input').val(null);
-    $('.alert').addClass('hide');
-    $('.alert').removeClass('show');
-    $('.alert').removeClass('alert-success');
-    $('.alert').removeClass('alert-danger');
-    location.reload();
-  });
 
 });
 
-$('.btn-save').on("click", (e) => {
-  // [data.customer_id, data.customer_name, data.kind_contact, data.sale_status, data.quantity,
-  // data.fee_ship, data.money_box, data.sum_money_order, data.create_at, data.phone_number, data.address, data.note, data.email_user_edit]
-  e.preventDefault();
-  let quantity = $('.quantity').find(":selected").val();
-  let fee_ship = parseInt($('.fee-ship').find(":selected").val());
-  let price = parseInt($('.price').val() === "" ? 0 : $('.price').val())
-  let money_box = parseInt(price) * parseInt(quantity);
-  let sum_money_order = money_box + fee_ship;
-  const dt = {
-    customer_id: $('.customer-id').val(),
-    customer_name: $('.customer-name').val(),
-    kind_contact: $('.kind-contact').find(":selected").val(),
-    sale_status: $('.sale-status').val(),
-    quantity: quantity,
-    fee_ship: fee_ship,
-    money_box: money_box,
-    sum_money_order: sum_money_order,
-    create_at: new Date().toLocaleString(),
-    phone_number: $('.phone-number').val(),
-    address: $(".address").val(),
-    note: $('.note').val(),
-    email_user_edit: "anonymos@gmail.com",
-    row: $('.row').val()
-  }
-
-  save_data(dt)
-
-})
-
-function save_data(data) {
-  let response;
-  if (data) {
-    $.ajax({
-      type: 'POST',
-      url: url_api,
-      dataType: 'json',
-      data: data,
-      success: function (data) {
-        if (data.status == 200) {
-          $('input').val(null);
-          $('.alert').removeClass('hide');
-          $('.alert').removeClass('alert-danger');
-          $('.alert').addClass('show alert-success');
-          $('.message').text("");
-          $('.message').text(data.message);
-        }
-        else {
-          $('.alert').removeClass('hide');
-          $('.alert').removeClass('alert-success');
-          $('.alert').addClass('show alert-danger');
-          $('.message').text("");
-          $('.message').text(data.message);
-        }
-      }
-
-    });
-
-  }
 
 
-}
--- crm.js
+++ crm.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     // Function show data to client
     function show_data_to_client(data) {
-        console.log(data);
         if (data) {
             $('#pagination').pagination({
                 dataSource: data,
@@ -21,7 +20,7 @@ $(document).ready(function () {
                             <td>${item.phone}</td>
                             <td><i class="far fa-check-circle ${ item.status ? 'status-checked' : 'status-check'}" title="${item.status ? 'Đã tạo vận đơn' : 'Chưa tạo vận đơn'}"></i></td>
                             
-                            <td value="${item.id_customer}"><a data-val="${item.id_customer}" data-toggle="modal" data-target="#exampleModal">${ item.status ? '' : '<i class="fas fa-plus-circle create-new-bill-order left" title="Tạo vận đơn"></i></a>'} 
+                            <td value="${item.id_customer}"><a data-val="${item.id_customer}" data-toggle="modal" data-target="#exampleModal">${item.status ? '' : '<i class="fas fa-plus-circle create-new-bill-order left" title="Tạo vận đơn"></i></a>'} 
                             <i class="fas fa-cog edit-order" title="Chỉnh sửa đơn hàng"></i>
                             <i class="far fa-trash-alt delete-order " title="Xóa đơn hàng">delete</i> </td>
                         </tr>
@@ -34,6 +33,32 @@ $(document).ready(function () {
         }
 
     }
+
+    $('#exampleModal').on('show.bs.modal', function (event) {
+        const order_id = $(event.relatedTarget).data('val');
+        // set value to input form
+        let data_find = JSON.parse(localStorage.getItem("data"));
+        let obj = data_find.filter(item => item.id_customer === order_id);
+        $('.customer-id').val(obj[0].id_customer);
+        $('.customer-name').val(obj[0].name);
+        $('.phone-number').val(obj[0].phone);
+        $('.name-sale').val(obj[0].name_sale_process);
+        $('.row').val(obj[0].row);
+    });
+
+    // Clear all value in modal when modal hide
+    $("#exampleModal").on('hide.bs.modal', function () {
+        $('input').val(null);
+        $('.alert').addClass('hide');
+        $('.alert').removeClass('show');
+        $('.alert').removeClass('alert-success');
+        $('.alert').removeClass('alert-danger');
+        get_data();
+        let data = JSON.parse(localStorage.getItem("data"));
+        first_load_data_to_front_end(data);
+
+    });
+
     // Function filter data search
     function flter_data(keyword, type) {
         const data = JSON.parse(localStorage.getItem("data"));
@@ -43,19 +68,25 @@ $(document).ready(function () {
             // search filter follow name customer
             if (type === "name_cus") {
                 jsonArr = data.filter(item => item.name === keyword)
-
                 // jsonArr.push(obj);
             } // Search follow with Order ID
             else if (type === "order_id") {
                 jsonArr = data.filter(item => item.id_customer === keyword)
 
             } // Search follow with Phone
-            else {
+            else if (type === "phone") {
                 jsonArr = data.filter(item => item.phone === keyword)
 
             }
+
         }
+        if (keyword == "") {
+            jsonArr = data;
+        }
+
+
         // Call function show data to client
+        // console.log(typeof keyword);
         show_data_to_client(jsonArr);
         // data.filter(x => x.name === "Blofeld");
     }
@@ -63,9 +94,7 @@ $(document).ready(function () {
     $('#key_search').keyup(e => {
         e.stopImmediatePropagation();
         let type_search = $('.type-search').find(":selected").val();
-        if (e.target.value !== "") {
-            flter_data(e.target.value, type_search);
-        }
+        flter_data(e.target.value, type_search);
 
     });
 
@@ -118,7 +147,7 @@ function load_data_to_front_end() {
                         <td>${item.name}</td>
                         <td>${item.phone}</td>
                         <td><i class="far fa-check-circle ${ item.status ? 'status-checked' : 'status-check'}" title="${item.status ? 'Đã tạo vận đơn' : 'Chưa tạo vận đơn'}"></i></td>  
-                        <td value="${item.id_customer}"> <a data-val="${item.id_customer}" data-toggle="modal" data-target="#exampleModal">${ item.status ? '' :'<i class="fas fa-plus-circle create-new-bill-order left" title="Tạo vận đơn"></i></a>'}  
+                        <td value="${item.id_customer}"> <a data-val="${item.id_customer}" data-toggle="modal" data-target="#exampleModal">${item.status ? '' : '<i class="fas fa-plus-circle create-new-bill-order left" title="Tạo vận đơn"></i></a>'}  
                         <i class="fas fa-cog edit-order" title="Chỉnh sửa đơn hàng"></i>
                         <i class="far fa-trash-alt delete-order " title="Xóa đơn hàng">delete</i> </td>
                         
@@ -132,7 +161,6 @@ function load_data_to_front_end() {
     }
 
 }
-
 function first_load_data_to_front_end(data) {
     // 
 
@@ -154,7 +182,7 @@ function first_load_data_to_front_end(data) {
                         <td>${item.name}</td>
                         <td>${item.phone}</td>
                         <td><i class="far fa-check-circle ${ item.status ? 'status-checked' : 'status-check'}" title="${item.status ? 'Đã tạo vận đơn' : 'Chưa tạo vận đơn'}"></i></td>
-                        <td value="${item.id_customer}"><a data-val="${item.id_customer}" data-toggle="modal" data-target="#exampleModal">${ item.status ? '' : '<i  class="fas fa-plus-circle create-new-bill-order left" title="Tạo vận đơn"></i></a>'}  
+                        <td value="${item.id_customer}"><a data-val="${item.id_customer}" data-toggle="modal" data-target="#exampleModal">${item.status ? '' : '<i  class="fas fa-plus-circle create-new-bill-order left" title="Tạo vận đơn"></i></a>'}  
                         <i class="fas fa-cog edit-order " title="Chỉnh sửa đơn hàng"></i>
                         <i class="far fa-trash-alt delete-order " title="Xóa đơn hàng"></i> </td>
                     </tr>
@@ -167,22 +195,94 @@ function first_load_data_to_front_end(data) {
     }
 
 }
-
-
-
-
 // function
+$('.btn-save').on("click", (e) => {
+    // [data.customer_id, data.customer_name, data.kind_contact, data.sale_status, data.quantity,
+    // data.fee_ship, data.money_box, data.sum_money_order, data.create_at, data.phone_number, data.address, data.note, data.email_user_edit]
+    e.preventDefault();
+    let quantity = $('.quantity').find(":selected").val();
+    let fee_ship = parseInt($('.fee-ship').find(":selected").val());
+    let price = parseInt($('.price').val() === "" ? 0 : $('.price').val())
+    let money_box = parseInt(price) * parseInt(quantity);
+    let sum_money_order = money_box + fee_ship;
+    const dt = {
+        customer_id: $('.customer-id').val(),
+        customer_name: $('.customer-name').val(),
+        kind_contact: $('.kind-contact').find(":selected").val(),
+        sale_status: $('.sale-status').val(),
+        quantity: quantity,
+        fee_ship: fee_ship,
+        money_box: money_box,
+        sum_money_order: sum_money_order,
+        create_at: new Date().toLocaleString(),
+        phone_number: $('.phone-number').val(),
+        address: $(".address").val(),
+        note: $('.note').val(),
+        email_user_edit: "anonymos@gmail.com",
+        row: $('.row').val()
+    }
+
+    save_data(dt)
+
+})
+
+function save_data(data) {
+
+    if (data) {
+        $.ajax({
+            type: 'POST',
+            url: url_api,
+            dataType: 'json',
+            data: data,
+            success: function (data) {
+                if (data.status == 200) {
+                    $('input').val(null);
+                    $('.alert').removeClass('hide');
+                    $('.alert').removeClass('alert-danger');
+                    $('.alert').addClass('show alert-success');
+                    $('.message').text("");
+                    $('.message').text(data.message);
+                }
+                else {
+                    $('.alert').removeClass('hide');
+                    $('.alert').removeClass('alert-success');
+                    $('.alert').addClass('show alert-danger');
+                    $('.message').text("");
+                    $('.message').text(data.message);
 
 
-//     // POST data
-//     fetch(url, {
-//         method: "GET",
-//         mode: 'no-cors',
-//         headers: {
-//             'Content-Type': 'applycation/json'
-//     },
-//         redirect: 'follow',
-//         body: JSON.stringify({ name: "Oni chan" })
-// });
+                }
+            }
 
+        });
 
+    }
+}
+//Chức năng lọc trên table data
+function filter_status(kind_status) {
+    const data = JSON.parse(localStorage.getItem("data"));
+    //   console.log(data);
+    let jsonArr;
+    if (kind_status === "1") {
+        jsonArr = data.filter(item => item.status === true)
+        // jsonArr.push(obj);
+    } // Search follow with Order ID
+    else if (kind_status === "2") {
+        jsonArr = data.filter(item => item.status === false)
+
+    } 
+    else{
+        jsonArr = data;
+    }
+  
+    // Call function show data to client
+    first_load_data_to_front_end(jsonArr);
+
+}
+// Event change select filter data
+$('.filter-status').on('change', e => {
+    let type_filter = $('.filter-status').find(":selected").val();
+    if (filter_status !== "") {
+        filter_status(type_filter);
+    }
+})
