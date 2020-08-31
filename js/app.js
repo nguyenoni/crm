979a82242cef22584f153e2fff452e5c37a8febd
nguyen oni <an64440@gmail.com>
2020-08-31 08:06:46 +0700

    a

--- js/app.js
+++ js/app.js
@@ -2,19 +2,12 @@
 
 const url_api = "https://script.google.com/macros/s/AKfycbwvWDwAkJu--B1sJfMS3jJSt2kKC2johynPPN9YxEtlXHuGAJl6/exec";
 
-
 $(document).ready(function () {
-  // function show data
-
-
 
   // Fakes the loading setting a timeout
   setTimeout(function () {
     $('body').addClass('loaded');
   }, 3500);
-  // Event load modal add new bill order
-  // $('.create-bill-order').on("click", )
-
 
 });
 
